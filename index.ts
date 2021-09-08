30888bb12e31d42e5b4884030af1fb3143357535
Jason Antwi-Appah <hey@jasonaa.me>
2021-09-08 13:25:03 -0500

    feat: actually make the thing

---
+++ index.ts
@@ -0,0 +1,62 @@
+(async () => {
+	const axios = require("axios").default;
+	const puppeteer = require("puppeteer");
+	const FormData = require("form-data");
+	const Blob = require("node-blob");
+	require("dotenv").config();
+
+	const hide = (el) => {
+		el.evaluate((e: HTMLElement) => (e.style.display = "none"));
+	};
+	const { HAC_USER, HAC_PASS, HAC_URL, CLOWN_UID, SLACK_TOKEN, CLOWN_CHANNEL } =
+		process.env;
+	const browser = await puppeteer.launch({
+		headless: process.env.NODE_ENV === "production",
+	});
+	try {
+		const page = await browser.newPage();
+		await page.setViewport({
+			width: 1920,
+			height: 1080,
+			deviceScaleFactor: 1,
+		});
+		await page.goto(HAC_URL);
+		await page.type("#LogOnDetails_UserName", HAC_USER);
+		await page.type("#LogOnDetails_Password", HAC_PASS);
+		await page.click("#login");
+		await page.waitForNavigation();
+		const [teachers, classIds] = await Promise.all([
+			page.$$("#staffName"),
+			page.$$("table > tbody > tr > td:nth-child(1) > div > div"),
+		]);
+		teachers.forEach(hide);
+		classIds.forEach(hide);
+		const img: Buffer = await page.screenshot({ type: "png" });
+
+		const fd = new FormData();
+		const filename = "grades.png"
+		fd.append("file", img, filename);
+		fd.append("initial_comment", `lmaooooo look at <@${CLOWN_UID}>'s grades`);
+		fd.append("filetype", "png");
+		fd.append("channels", CLOWN_CHANNEL);
+		fd.append("filename", filename);
+		fd.append("title", "ahahahaha");
+
+		await axios.post("https://slack.com/api/files.upload", fd, {
+			headers: {
+				Authorization: `Bearer ${SLACK_TOKEN}`,
+				...fd.getHeaders(),
+			},
+		});
+	} catch (e) {
+		console.error(e);
+	} finally {
+		await browser.close();
+	}
+})();
+
+async function wait(ms: number) {
+	return new Promise((res) => {
+		setTimeout(res, ms);
+	});
+}
